26f393093a21ce3bf42ceaeb790042ef76bcc065
MaminGansta <sa00709@mail.ru>
2020-01-02 13:48:32 +0300

    texture

--- learnOpenGL/openGL/main.cpp
+++ learnOpenGL/openGL/main.cpp
@@ -1,6 +1,7 @@
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <SOIL.h>
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -16,19 +17,29 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 
 
 // Shaders
-const GLchar* vectexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 position;\n"
+const GLchar* vectexShaderSource = 
+    "#version 330 core\n"
+    "layout(location = 0) in vec3 position;\n"
+    "layout(location = 1) in vec3 color;   \n"
+    "layout(location = 2) in vec2 texCoord;\n"
+    "out vec3 ourColor;\n"
+    "out vec2 TexCoord;\n"
     "void main()\n"
     "{\n"
-    "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
-    "}\0";
-const GLchar* fragmentShaderSource = "#version 330 core\n"
-    "out vec4 color;\n"
-    "void main()\n"
-    "{\n"
-    "color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\n\0";
-
+    "    gl_Position = vec4(position, 1.0f);\n"
+    "    ourColor = color;\n"
+    "    TexCoord = texCoord;\n"
+    "}";
+const GLchar* fragmentShaderSource =
+    "#version 330 core                             \n"
+    "in vec3 ourColor;                             \n"
+    "in vec2 TexCoord;                             \n"
+    "out vec4 color;                               \n"
+    "uniform sampler2D ourTexture;                 \n"
+    "void main()                                   \n"
+    "{                                             \n"
+    "    color = texture(ourTexture, TexCoord);    \n"
+    "}                                             \n";
 
 int main(void)
 {
@@ -121,17 +132,27 @@ int main(void)
     glDeleteShader(fragmentShader);
 
     // Set up vertex data (and buffer(s)) and attribute pointers
+    //GLfloat vertices[] = {
+    //     0.5f,  0.5f, 0.0f,  // Top Right
+    //     0.5f, -0.5f, 0.0f,  // Bottom Right
+    //    -0.5f, -0.5f, 0.0f,  // Bottom Left
+    //    -0.5f,  0.5f, 0.0f   // Top Left 
+    //};
+
     GLfloat vertices[] = {
-         0.5f,  0.5f, 0.0f,  // Top Right
-         0.5f, -0.5f, 0.0f,  // Bottom Right
-        -0.5f, -0.5f, 0.0f,  // Bottom Left
-        -0.5f,  0.5f, 0.0f   // Top Left 
+        // Позиции          // Цвета             // Текстурные координаты
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // Верхний правый
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // Нижний правый
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // Нижний левый
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // Верхний левый
     };
     GLuint indices[] = {  // Note that we start from 0!
         0, 1, 3,  // First Triangle
         1, 2, 3   // Second Triangle
     };
 
+
+    // VAO
     GLuint VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -145,27 +166,61 @@ int main(void)
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    // vertex attrib
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
+
+    // color attrib
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    // texture attrib
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(2);
 
     // unbind buffer and array
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0); 
 
 
+    // load image
+    int img_width, img_height;
+    unsigned char* image = SOIL_load_image("container.jpg", &img_width, &img_height, 0, SOIL_LOAD_RGB);
+    if (image == NULL || img_height == 0)
+    {
+        printf("image failled");
+        return 1;
+    }
+
+    // texture generation
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img_width, img_height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    // free data
+    SOIL_free_image_data(image);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+
+
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
-
-
+        
         // draw
         // clear screen
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Draw triangle
         //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         glUseProgram(shaderProgram);
+
+        // bind texture;
+        glBindTexture(GL_TEXTURE_2D, texture);
+
         glBindVertexArray(VAO);
         //glDrawArrays(GL_TRIANGLES, 0, 3);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
