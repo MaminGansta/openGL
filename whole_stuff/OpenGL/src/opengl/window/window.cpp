a0e6b1a94c3999210607a6bb6fd65e8ece269714
mamingansta <sa00709@mail.ru>
2020-07-07 23:26:14 +0300

    make window resizable

--- whole_stuff/OpenGL/src/opengl/window/window.cpp
+++ whole_stuff/OpenGL/src/opengl/window/window.cpp
@@ -80,7 +80,7 @@ namespace gl
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-        glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+        //glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
         
         m_Window = glfwCreateWindow(width, height, name, NULL, NULL);
